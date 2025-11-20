7921383916f99b9e37576825f4f663eea97972a4
ricozack97 <ricoditio12@gmail.com>
2025-11-20 12:36:14 +0700

    selesaikan konflik merge

--- js/features/placeholder.js
+++
@@ -1,8 +0,0 @@
-// js/features/placeholder.js
-
-export default function () {
-  document.getElementById('content-area').innerHTML = `
-    <h2>Menu belum diatur</h2>
-    <p>Silakan memilih menu yang sudah tersedia.</p>
-  `;
-}
